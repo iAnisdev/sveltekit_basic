46be45a15f2e6a4bb9cf0d28fa87c6f0950fb09e
marahmanjs <imarahmandev@gmail.com>
2021-11-17 00:38:41 +0500

    layout and routing

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+    export async function load(ctx){
+        console.log("context " , ctx)
+
+        return {
+
+        }
+    }
+
+</script>
+
+<script lang="ts">
+    let name: string = 'HomePage'
+</script>
+
+<div>
+    <h4>{name}</h4>
+    <p>Hello theree </p>
+</div>
+
+
+<style>
+    p{
+        color: green;
+    }
+</style>
